6eccc7c7515c3c9331bd1abb47bf8f87f1484734
Chano Vera <contacto@chanovera.com>
2023-06-23 13:31:15 -0600

    Ingresa dos números entre 17 y 3, te entregaré el cociente, el residuo y su número real.

--- divisionNumeros/src/divisionNumeros/DivisionNumeros.java
+++ divisionNumeros/src/divisionNumeros/DivisionNumeros.java
@@ -1,18 +1,44 @@
 package divisionNumeros;
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class DivisionNumeros {
 
 	float num1, num2, cociente, residuo;
 	int residuo2;
 	double numeroReal;
+	String texto;
+	boolean validacion1 = false;
+	boolean validacion2 = false;
 	
 	public void obtenerDatos() {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Ingresa dos números entre 17 y 3, te entregaré el cociente, el residuo y su número real.\n\nIngresa los números:");
-		num1 = input.nextFloat();
-		num2 = input.nextFloat();
+		
+		System.out.println("Ingresa dos números entre 17 y 3, te entregaré el cociente, el residuo y su número real.\n\nIngresa el primer número:");
+		
+		while (!validacion1) {
+            if (input.hasNextFloat()) {
+                num1 = input.nextFloat();
+                validacion1 = true;
+            } else {
+                texto = input.next();
+                System.out.println("Has ingresado un texto, por favor, ingresa un número.");
+            }
+        }
+		
+		System.out.println("Ingresa el segundo número:");
+
+		while (!validacion2) {
+            if (input.hasNextFloat()) {
+                num2 = input.nextFloat();
+                validacion2 = true;
+            } else {
+                texto = input.next();
+                System.out.println("Has ingresado un texto, por favor, ingresa un número.");
+            }
+        }
+		
 	}
 	
 	public void cociente() {
@@ -35,6 +61,35 @@ public class DivisionNumeros {
 		operacion.residuo();
 		operacion.real();
 		
-		System.out.println("El cociente de " + operacion.num1 + " entre " + operacion.num2 + " es " + operacion.cociente + ".\nSu residuo es " + operacion.residuo + ".\nSu número real es " + operacion.numeroReal);
+		DecimalFormat decimalFormat = new DecimalFormat("#.##########");
+		
+		String num1Formateado = decimalFormat.format(operacion.num1);
+		String num2Formateado = decimalFormat.format(operacion.num2);
+        String cocienteFormateado = decimalFormat.format(operacion.cociente);
+        String residuoFormateado = decimalFormat.format(operacion.residuo);
+        String realFormateado = decimalFormat.format(operacion.numeroReal);
+        
+        if (!num1Formateado.endsWith(".0000000000")) {
+        	num1Formateado = decimalFormat.format(operacion.num1);
+        }
+        
+        if (!num2Formateado.endsWith(".0000000000")) {
+        	num2Formateado = decimalFormat.format(operacion.num2);
+        }
+        
+        if (!cocienteFormateado.endsWith(".0000000000")) {
+        	cocienteFormateado = decimalFormat.format(operacion.cociente);
+        }
+        
+        if (!residuoFormateado.endsWith(".0000000000")) {
+        	residuoFormateado = decimalFormat.format(operacion.residuo);
+        }
+        
+        if (!realFormateado.endsWith(".0000000000")) {
+        	realFormateado = decimalFormat.format(operacion.numeroReal);
+        }
+        
+		
+		System.out.println("El cociente de " + num1Formateado + " entre " + num2Formateado + " es " + cocienteFormateado + ".\nSu residuo es " + residuoFormateado + ".\nSu número real es " + realFormateado);
 	}
 }
