6fd9e492f901822b97d41833cb54f6b2a831b902
Chano Vera <contacto@chanovera.com>
2023-06-23 13:25:56 -0600

    Calcula el promedio de numeros flotantes usando clases

---
+++ promedioClases/src/promedioClases/PromedioClases.java
@@ -0,0 +1,32 @@
+package promedioClases;
+
+import java.util.Scanner;
+
+public class PromedioClases {
+	
+	public static void main(String[] args) {
+        PromedioClases promedio = new PromedioClases();
+        
+        promedio.obtenerDatos();
+   
+    }
+	
+	public double obtenerDatos() {
+		
+		Scanner input = new Scanner(System.in);
+		int contador = 1;
+		double suma = 0;
+		
+		while (contador <= 5) {
+            System.out.print("Ingresa el dato #" + contador + ": ");
+            int numero = input.nextInt();
+            suma += numero;
+            contador++;
+        }
+
+        double promedio = suma / 5.0;
+        System.out.println("El promedio de los datos ingresados es: " + promedio);
+        return promedio;
+	}
+
+}
