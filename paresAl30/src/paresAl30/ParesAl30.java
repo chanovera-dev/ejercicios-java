bf2d0a8b7dcab5d417ac474bf3f7eaa1ac5a72ca
Chano Vera <contacto@chanovera.com>
2023-06-27 17:40:07 -0600

    Imprimir los números pares del 1 al 30

---
+++ paresAl30/src/paresAl30/ParesAl30.java
@@ -0,0 +1,13 @@
+package paresAl30;
+
+public class ParesAl30 {
+	
+	public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+	
+}
